2041feabb9e3857615beafc19cd9af57afeb081c
joshua murray <joshuajmurray@gmail.com>
2017-11-15 22:05:06 -0800

    last commit for chap 20

--- app/src/main/java/com/jmurray/android/beatbox/SoundViewModel.java
+++ app/src/main/java/com/jmurray/android/beatbox/SoundViewModel.java
@@ -1,10 +1,13 @@
 package com.jmurray.android.beatbox;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /**
  * Created by joshu on 11/14/2017.
  */
 
-public class SoundViewModel {
+public class SoundViewModel extends BaseObservable {
     private Sound mSound;
     private BeatBox mBeatBox;
 
@@ -16,11 +19,13 @@ public class SoundViewModel {
         return mSound;
     }
 
+    @Bindable
     public String getTitle() {
         return mSound.getmName();
     }
 
     public void setSound(Sound sound) {
         mSound = sound;
+        notifyChange();
     }
 }
