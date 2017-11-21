5634d66468491c6455199b8ea16c8a5ed6c4a89c
joshua murray <joshuajmurray@gmail.com>
2017-11-20 22:44:21 -0800

    add soundviewModelTest

---
+++ app/src/test/java/com/jmurray/android/beatbox/SoundViewModelTest.java
@@ -0,0 +1,31 @@
+package com.jmurray.android.beatbox;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created by joshua on 11/20/2017.
+ */
+public class SoundViewModelTest {
+    private BeatBox mBeatbox;
+    private Sound mSound;
+    private SoundViewModel mSubject
+
+    @Before
+    public void setUp() throws Exception {
+        mBeatbox = mock(BeatBox.class);
+        mSound = new Sound("assettPath");
+        mSubject = new SoundViewModel(mBeatbox);
+        mSubject.setSound(mSound);
+    }
+
+    @Test
+    public void exposesSoundNameAsTitle() {
+        assertThat(mSubject.getTitle(), is(mSound.getmName()));
+    }
+
+}
